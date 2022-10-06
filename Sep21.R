8672e0ae0a6ca45a2f61c7a73bd0f0e213a1fd3d
Davion Greene <112106262+davion316@users.noreply.github.com>
2022-10-05 21:19:51 -0400

    Create Sep21.R

---
+++ Sep21.R
@@ -0,0 +1,10 @@
+# Author: Greene, Date: 09/21/2022, Purpose: Perform ANOVA analysis on sample dataset
+
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv" 
+
+df <- read.csv(PATH)
+
+df
+
+anova_one_way <- aov(time~poison, data = df);
+summary(anova_one_way)
